1104be4086b347d8abf1135507124f34246990a0
OmarAdamMostafa <115920338+OmarAdamMostafa@users.noreply.github.com>
2022-12-03 17:25:59 +0200

    Commit #1.2
    - Added Categories and CategoryItem components.
    - Updated data.js with categories array.

---
+++ src/components/Categories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import { categories } from '../data'
+import CategoryItem from './CategoryItem'
+
+const Categories = () => {
+  return (
+    <Container>
+        {
+            categories.map((item)=>{
+                return <CategoryItem item={item}/>
+            })
+        }
+    </Container>
+  )
+}
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+`
+
+export default Categories
